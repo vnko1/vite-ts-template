4b4301691d3654378131ac2dfcf5beb2ecc736f0
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-26 07:51:01 +0200

    feat: add presets

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,25 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
 
-
-export default defineConfig({ plugins: [] });
+export default defineConfig({
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "src", "partials"),
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "nested/index.html"),
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname) },
+      { find: "src", replacement: resolve(__dirname, "src") },
+    ],
+  },
+});
