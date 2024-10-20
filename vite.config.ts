f974b769aedbe611f798a8b1af2affc09ff59adb
Andrii Valenko <andreyvalenko@gmail.com>
2024-10-20 12:19:51 +0300

    updated: deps versions; changed: deps, gitignore and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
-  plugins: [
-    handlebars({
-      partialDirectory: resolve(__dirname, "src", "partials"),
-    }),
-  ],
+  plugins: [],
   build: {
     rollupOptions: {
       input: {
